2aa5f04a4acb7ac4a7eaded07c080f414041af0d
luminoux <yungzin98@naver.com>
2022-05-29 15:52:59 +0900

    chore : Add project
    프로젝트를 추가했습니다.
    확인부탁드립니다.
    
    Co-Authored-By: Asher <85096805+Asher3576@users.noreply.github.com>
    Co-Authored-By: Chanhee Jeong <63157395+chaneeii@users.noreply.github.com>
    Co-Authored-By: Youth <99013115+kimscastle@users.noreply.github.com>
    Co-Authored-By: Charlie <88371913+LentoAssai@users.noreply.github.com>
    Co-Authored-By: hankyulee <77485339+kyustudyo@users.noreply.github.com>

---
+++ DonWorry/DonWorry/DonWorryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonWorryApp.swift
+//  DonWorry
+//
+//  Created by YeongJin Jeong on 2022/05/29.
+//
+
+import SwiftUI
+
+@main
+struct DonWorryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
