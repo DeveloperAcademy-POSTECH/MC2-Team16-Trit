9d75f93bf72ac10cc6380c1f6ab7689f7d366fce
Chanhee Jeong <chan420420@gmail.com>
2022-06-01 22:24:42 +0900

    Add : Color Extension
    Color 라이브러리 공통 사용을 위한 Color Extension 추가

---
+++ DonWorry/DonWorry/Extension/ColorExtension.swift
@@ -0,0 +1,52 @@
+//
+//  File.swift
+//  DonWorry
+//
+//  Created by Chanhee Jeong on 2022/06/01.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    init(hex: String){
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        
+        let r = Double((rgb >> 16) & 0xFF) / 255.0
+        let g = Double((rgb >> 8) & 0xFF) / 255.0
+        let b = Double((rgb >> 0) & 0xFF) / 255.0
+        self.init(red: r, green: g, blue: b)
+        
+    }
+    
+    
+    /** CUSTOMISED COLOR **/
+    
+    /*
+    // 1. HEX 사용법
+    static let grayC5 = Color(hex: "#c5c5c5")
+    // 2. UIColor 사용법
+    static let background = Color(UIColor.systemBlue)
+    // 3. 시스템 이름으로 된 컬러 사용법
+    static let bieberPeach = Color("peach")
+    // 4. 프라이머리 컬러에 오퍼시티까지 !
+    static let primaryShadoe = Color.primary.opacity(0.2)
+     */
+    
+    
+    // ADVENT COLOR
+    static let blueMain = Color(hex: "#1C6BFF")
+    static let black00 = Color(hex: "#000000")
+    static let grayC5 = Color(hex: "#c5c5c5")
+    static let gray81 = Color(hex: "#818181")
+    static let gray75 = Color(hex: "#757575")
+    static let grayEF = Color(hex: "#EFEFEF")
+    static let grayEE = Color(hex: "#EEEEEE")
+    static let grayBC = Color(hex: "#BCBCBC")
+    static let grayD8 = Color(hex: "#D8D8D8")
+    
+}
