d6cfe52cdb282ef0b9840c64983a81b62605608a
LentoAssai <88371913+LentoAssai@users.noreply.github.com>
2022-06-02 15:20:26 +0900

    Feat: Add UnderlineTextField
    UnderlineTextField를 추가했습니다.
    - placeholder, maxLength 설정
    - 전체 삭제 기능
    - 글자 수 제한

---
+++ DonWorry/DonWorry/Resources/UnderlineTextField.swift
@@ -0,0 +1,63 @@
+//
+//  UnderlineTextField.swift
+//  DonWorry
+//
+//  Created by 김승창 on 2022/06/02.
+//
+
+import Combine
+import SwiftUI
+
+struct UnderlineTextField: View {
+    let placeholder: String
+    let maxLength: Int
+    
+    @State private var text = ""
+    
+    var body: some View {
+        VStack {
+            HStack {
+                TextField(placeholder, text: $text)
+                    .frame(height: 30)
+                    .textInputAutocapitalization(.never)
+                    .onReceive(Just(text), perform: { _ in
+                        if maxLength < text.count {
+                            text = String(text.prefix(maxLength))
+                        }
+                    })
+                
+                if !text.isEmpty {
+                    Image(systemName: "xmark.circle.fill")
+                        .imageScale(.medium)
+                        .foregroundColor(Color(.systemGray3))
+                        .padding(5)
+                        .onTapGesture {
+                            withAnimation {
+                                text.removeAll()
+                            }
+                        }
+                }
+            }
+            
+            Divider()
+                .frame(height: 1)
+                .padding(.horizontal, 40)
+                .background(.blue)
+            
+            HStack {
+                Spacer()
+                Text("\(text.count)/\(maxLength)")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                    .padding(.trailing)
+            }
+        }
+        .frame(width: 350)
+    }
+}
+
+struct UnderlineTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        UnderlineTextField(placeholder: "placeholder", maxLength: 20)
+    }
+}
