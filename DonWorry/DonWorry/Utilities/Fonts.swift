818751abac832e2e23f6dc7571c21ac754bd9ddc
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-06-06 19:24:43 +0900

    add: Add fonts

---
+++ DonWorry/DonWorry/Utilities/Fonts.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftUIView.swift
+//  DonWorry
+//
+//  Created by 임영후 on 2022/06/03.
+//
+
+import SwiftUI
+
+// 사용예시
+// Text("sssd")
+//    .scaledFont(name: CustomFont.GmarketSansTTFBold, size: 17)
+
+struct CustomFont {
+    static let GmarketSansBold = "GmarketSansTTFBold"
+    static let GmarketSansLight = "GmarketSansTTFLight"
+}
+
+@available(iOS 13, macCatalyst 13, tvOS 13, watchOS 6, *)
+struct ScaledFont: ViewModifier {
+    @Environment(\.sizeCategory) var sizeCategory
+    var name: String
+    var size: CGFloat
+
+    func body(content: Content) -> some View {
+       let scaledSize = UIFontMetrics.default.scaledValue(for: size)
+        return content.font(.custom(name, size: scaledSize))
+    }
+}
+
+@available(iOS 13, macCatalyst 13, tvOS 13, watchOS 6, *)
+extension View {
+    func scaledFont(name: String, size: CGFloat) -> some View {
+        return self.modifier(ScaledFont(name: name, size: size))
+    }
+}
---
+++ DonWorry/DonWorry/Views/CardCreationView/Card/PreviewCardView.swift
@@ -0,0 +1,37 @@
+//
+//  PreviewCardView.swift
+//  DonWorry
+//
+//  Created by 임영후 on 2022/06/03.
+//
+
+import SwiftUI
+
+struct PreviewCardView: View {
+    
+    @Binding var account: String
+    @Binding var color: CardColor
+    let date: String
+    @Binding var image: [UIImage]
+    
+    @Binding var decoCase: DecoCase
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+//            SpaceMainCardView(index: 0, clicked: {
+//                
+//            }, isParticipated: false)
+//            CardView(color: color.color, account: account, date: date)
+            SpaceMainCardView(color: color.color, account: account, index: 0, clicked: {
+                
+            }, isParticipated: false, date: date)
+        }
+        .padding()
+    }
+}
+
+// struct PreviewCardView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PreviewCardView()
+//    }
+// }
