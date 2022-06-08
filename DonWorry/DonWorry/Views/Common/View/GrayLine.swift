212e797edbbce9bf25731090529d7c358337e87f
Chanhee Jeong <chan420420@gmail.com>
2022-06-08 10:29:57 +0900

    Feat : Replace Line with GrayLine

---
+++ DonWorry/DonWorry/Views/Common/View/GrayLine.swift
@@ -0,0 +1,23 @@
+//
+//  GrayLine.swift
+//  DonWorry
+//
+//  Created by Chanhee Jeong on 2022/06/08.
+//
+
+import SwiftUI
+
+struct GrayLine: View {
+    var body : some View {
+        Rectangle()
+            .frame(height: 0.5, alignment: .bottom)
+            .foregroundColor(.grayC5)
+    }
+}
+
+struct GrayLine_Previews: PreviewProvider {
+    static var previews: some View {
+        GrayLine()
+    }
+}
+
