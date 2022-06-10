eeb3e4d77b7b451173e5444a0b7176d4ddbefb45
luminoux <yungzin98@naver.com>
2022-06-10 22:04:00 +0900

    Feat: User CRUD 기능 추가
    + FIreStore의 collection "User"에 접근, CRUD할 수 있는 기능을 추가했습니다.

--- DonWorry/DonWorry/ViewModels/FireStoreViewModel.swift
+++ DonWorry/DonWorry/ViewModels/FireStoreViewModel.swift
@@ -6,3 +6,95 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseFirestore
+
+class FireStoreViewModel: ObservableObject {
+
+    @Published var list = [User]()
+
+    // 유저 삭제하기 함수
+    func deleteUserData(userToDelete: User) {
+
+        let db = Firestore.firestore() // FireBase 데이터 베이스를 reference
+        
+        // collection에 접근
+        db.collection("User").document(userToDelete.id).delete { error in
+            if error == nil {
+
+                DispatchQueue.main.async {
+                    self.list.removeAll { user in
+                        return user.id == userToDelete.id
+                    }
+                }
+
+            } else {
+                print("유저 삭제하기 실패")
+            }
+        }
+    }
+
+    // 유저 추가하기 함수
+    // 사용방법
+    // 1. User 구조체를 만들어 준다.
+    // 2. addUserData(구조체변수) -> db에 데이터가 추가 됩니다.
+    func addUserData(user: User) {
+        let db = Firestore.firestore()
+
+        db.collection("User").addDocument(data:["userName": user.userName,
+                                                "nickName": user.nickName,
+                                                "image": user.image,
+                                                "account": user.account,
+                                                "spaceList": user.spaceList]) { error in
+
+            if error == nil {
+                self.getUserData()
+            } else {
+                print("유저 추가하기 실패")
+            }
+        }
+    }
+
+    // 현재 유저 불러오기 함수
+    func getUserData() {
+        // get a reference to the database
+        let db = Firestore.firestore()
+        db.collection("User").getDocuments { snapshot, error in
+            if error == nil {
+
+                if let snapshot = snapshot {
+
+                    DispatchQueue.main.async {
+                        self.list = snapshot.documents.map { d in
+                            return User(id: d.documentID,
+                                        userName: d["userName"] as! String,
+                                        nickName: d["nickName"] as! String,
+                                        account: d["account"] as! String, // TODO: reference로 수정해야함
+                                        image: d["image"] as! String,
+                                        spaceList: d["spaceList"] as! [String]
+                            )
+                        }
+                    }
+                }
+
+            } else {
+                print("유저 불러오기 실패")
+            }
+        }
+    }
+
+    // 유저 정보 업데이트의 예시로 이름 바꾸기를 선택!
+    func updateUserName(userToUpdate: User, newName: String?) {
+
+        let db = Firestore.firestore()
+
+        db.collection("User").document(userToUpdate.id).setData(["userName" : newName ?? ""], merge: true) { error in
+
+            if error == nil {
+                self.getUserData()
+            } else {
+                print("유저 정보 업데이트 실패")
+            }
+        }
+    }
+}
