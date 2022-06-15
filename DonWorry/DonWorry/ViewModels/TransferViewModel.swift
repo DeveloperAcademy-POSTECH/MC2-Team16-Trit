d5b0b5fd4825d0da855ebca605474f59b30729c0
luminoux <yungzin98@naver.com>
2022-06-15 16:49:29 +0900

    Feat: func fetchTransferList & fetchTakingList
    작동이 잘 됩니다.

--- DonWorry/DonWorry/ViewModels/TransferViewModel.swift
+++ DonWorry/DonWorry/ViewModels/TransferViewModel.swift
@@ -19,22 +19,70 @@ class TransferViewModel: ObservableObject {
 //    @EnvironmentObject var authViewModel: AuthViewModel -> 다른 뷰 불러쓰기
     
     private var db = Firestore.firestore()
-    
-    
-    // MARK: 내가 속한 스페이스의 transfer 목록 부르기
+
+    // MARK: Transfer 만들기
+    func createTransfer(user: User) {
+        
+        let db = Firestore.firestore()
+        
+        let transfer = TransferModel(giver: "", taker: "", amount: 20000, spaceID: "")
+
+        do {
+            let _ = try db.collection("Transfer").addDocument(from: transfer)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
     // params
     // > from: spaceId (내가속한스페이스의ID)
-    func fetchTransferList() {
-        // 1. fetch 해서
-        // 2. transferList 설정
+    // MARK: 내가 속한 스페이스의 transfer 목록 부르기
+    func fetchTransferList(from: String) {
+        
+        let transferRef = db.collection("Transfer").whereField("spaceID", isEqualTo: from)
+        
+        transferRef.getDocuments { snapshot, error in
+            if let snapshot = snapshot {
+                DispatchQueue.main.async {
+                    self.transferList = snapshot.documents.map { d in
+                        print(d.documentID)
+                        return TransferModel(id: d.documentID,
+                                             giver: d["giver"] as! String,
+                                             taker: d["taker"] as! String,
+                                             amount: d["amount"] as! Int,
+                                             spaceID: d["spaceID"] as! String,
+                                             isCompleted: (d["isCompleted"] != nil))
+                    }
+                    print(self.transferList)
+                }
+            }
+        }
     }
     
     // MARK: 내가 속한 특정 스페이스에 대한 받을 돈 목록
     // params
     // > from: spaceId (내가속한스페이스의ID)
-    func fetchTakingList() {
-        // 1. fetch 해서
-        // 2. transferList 설정
+    func fetchTakingList(from: String, userID: String) {
+        let transferRef = db.collection("Transfer")
+            .whereField("spaceID", isEqualTo: from)
+            .whereField("taker", isEqualTo: userID)
+        
+        transferRef.getDocuments { snapshot, error in
+            if let snapshot = snapshot {
+                DispatchQueue.main.async {
+                    self.transferList = snapshot.documents.map { d in
+                        print(d.documentID)
+                        return TransferModel(id: d.documentID,
+                                             giver: d["giver"] as! String,
+                                             taker: d["taker"] as! String,
+                                             amount: d["amount"] as! Int,
+                                             spaceID: d["spaceID"] as! String,
+                                             isCompleted: (d["isCompleted"] != nil))
+                    }
+                    print(self.transferList)
+                }
+            }
+        }
     }
     
     /* TODO: 구현하기 ( parma 없이 이름 겹쳐서 주석)
