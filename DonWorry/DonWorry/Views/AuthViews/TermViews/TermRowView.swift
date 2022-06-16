9a15e49840563c600bd559890095c7ccefa9a6bc
Asher3576 <dud3576@naver.com>
2022-06-17 00:58:34 +0900

    Fix: TermRowView 수정

--- DonWorry/DonWorry/Views/AuthViews/TermViews/TermRowView.swift
+++ DonWorry/DonWorry/Views/AuthViews/TermViews/TermRowView.swift
@@ -11,51 +11,52 @@ struct TermRowView: View {
     @Binding var term: Term
     
     var body: some View {
-        
-        VStack {
-            HStack {
-                Button {
-                    withAnimation {
-                        term.isAgreed.toggle()
+        ScrollView {
+            VStack {
+                HStack {
+                    Button {
+                        withAnimation {
+                            term.isAgreed.toggle()
+                        }
+                    } label: {
+                        Image(systemName: "circle.fill")
+                            .foregroundColor(term.isAgreed ? Color.blueMain : Color.grayE7)
+                            .padding(.trailing, 5)
                     }
-                } label: {
-                    Image(systemName: "circle.fill")
-                        .foregroundColor(term.isAgreed ? Color.blueMain : Color.grayE7)
-                        .padding(.trailing, 5)
-                }
-                
-                Group {
-                    Text(term.isEssential ? "(필수)" : "(선택)")
-                    Text(term.title)
-                }
-                .font(.system(size: 15))
-                .foregroundColor(Color.black)
-                
-                Spacer()
-                
-                Button {
-                    withAnimation {
-                        term.showContent.toggle()
+                    
+                    Group {
+                        Text(term.isEssential ? "(필수)" : "(선택)")
+                        Text(term.title)
                     }
-                } label: {
-                    Image(systemName: term.showContent ? "chevron.up" : "chevron.down")
-                        .foregroundColor(Color.gray81)
-                }
-                
-            }
-            .frame(maxWidth: .infinity)
-            .padding(.horizontal)
-            .padding(.vertical, 5)
-            
-            if term.showContent {
-                HStack{
-                    Text(term.content)
-                        .multilineTextAlignment(.leading)
-                        .font(.system(size: 13))
-                        .foregroundColor(Color.gray81)
+                    .font(.system(size: 15))
+                    .foregroundColor(Color.black)
+                    
                     Spacer()
+                    
+                    Button {
+                        withAnimation {
+                            term.showContent.toggle()
+                        }
+                    } label: {
+                        Image(systemName: term.showContent ? "chevron.up" : "chevron.down")
+                            .foregroundColor(Color.gray81)
+                    }
+                    
                 }
+                .frame(maxWidth: .infinity)
                 .padding(.horizontal)
+                .padding(.vertical, 5)
+                
+                if term.showContent {
+                    HStack{
+                        Text(term.content)
+                            .multilineTextAlignment(.leading)
+                            .font(.system(size: 13))
+                            .foregroundColor(Color.gray81)
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                }
             }
         }
     }
