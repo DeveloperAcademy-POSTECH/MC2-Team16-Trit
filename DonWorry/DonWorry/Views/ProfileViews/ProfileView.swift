19dc280f770e8729d741862485cfa48820ac3492
Asher3576 <dud3576@naver.com>
2022-06-17 01:59:02 +0900

    Fix: ProfileView UI수정

--- DonWorry/DonWorry/Views/ProfileViews/ProfileView.swift
+++ DonWorry/DonWorry/Views/ProfileViews/ProfileView.swift
@@ -16,171 +16,184 @@ struct ProfileView: View {
     var bankAccountNumber: String = "0000000000000000"
     var bankAccountHolder: String = "홍길동"
     var profileImage = "people_ICON"
-        
+    
+    @State private var deleteAccount = false
+    @State private var showLogOutAlert = false // 스페이스 나가기 시 Alert를 보여주기 위한 변수
+    
     var body: some View {
-        
-        VStack {
-            /* Main Content */
+        ZStack {
             VStack {
-                /* Section 1 :  Profile */
-                HStack {
-                    Image(profileImage)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 50, height: 50)
-                        .background(.black)
-                        .clipShape(Circle())
-                    
-                    VStack(alignment: .leading) {
-                        Text("김유스 님")
-                            .font(.system(size: 20, weight: .bold))
-                        Text("안녕하세요")
-                            .font(.system(size: 17))
-                    }
-                    .padding(.leading, 10)
-                    
-                    Spacer()
-                    
-                    VStack {
-                        
-                        NavigationLink(destination: EditProfileView()) {
-                            Image(systemName: "pencil")
-                                .foregroundColor(Color.black)
-                        }
-                        
-                    }
-                }
-                .padding(.bottom)
-                
-                GrayLine()
-                
-                /* Section 2 : Bank Account */
+                /* Main Content */
                 VStack {
+                    /* Section 1 :  Profile */
                     HStack {
-                        Text("나의 계좌")
-                            .font(.system(size: 17))
+                        Image(profileImage)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 50, height: 50)
+                            .background(.black)
+                            .clipShape(Circle())
+                        
+                        VStack(alignment: .leading) {
+                            Text("김유스 님")
+                                .font(.system(size: 20, weight: .bold))
+                            Text("안녕하세요")
+                                .font(.system(size: 17))
+                        }
+                        .padding(.leading, 10)
+                        
                         Spacer()
                         
                         VStack {
                             
-                            NavigationLink(destination: EditAccountView()) {
+                            NavigationLink(destination: EditProfileView()) {
                                 Image(systemName: "pencil")
                                     .foregroundColor(Color.black)
                             }
                             
                         }
-                        
                     }
-                    .padding(.bottom, 10)
+                    .padding(.bottom)
+                    
+                    GrayLine()
+                    
+                    /* Section 2 : Bank Account */
+                    VStack {
+                        HStack {
+                            Text("나의 계좌")
+                                .font(.system(size: 17))
+                            Spacer()
+                            
+                            VStack {
+                                
+                                NavigationLink(destination: EditAccountView()) {
+                                    Image(systemName: "pencil")
+                                        .foregroundColor(Color.black)
+                                }
+                                
+                            }
+                            
+                        }
+                        .padding(.bottom, 10)
+                        
+                        Button {
+                            
+                            print("계좌번호가 복사되었습니다.")
+                            
+                        } label: {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(bankAccountBank)
+                                        .bold()
+                                        .font(.system(size: 13))
+                                        .foregroundColor(Color.grayAccount)
+                                        .padding(.bottom, 5)
+                                    
+                                    HStack {
+                                        Text(bankAccountNumber)
+                                            .font(.system(size: 13))
+                                            .foregroundColor(Color.grayAccount)
+                                        Text(bankAccountHolder)
+                                            .font(.system(size: 13))
+                                            .foregroundColor(Color.grayAccount)
+                                    }
+                                }
+                                Spacer()
+                                Image(systemName: "doc.on.doc")
+                                    .foregroundColor(.grayWithBlue)
+                            }
+                            .padding(20)
+                            .frame(width: 340, height: 90, alignment: .leading)
+                            .background(Color.grayF0)
+                            .cornerRadius(8)
+                        }
+                    }
+                    .padding(.vertical, 10)
+                    GrayLine()
+                    
+                    /* Section 3 : 공지사항, 이용약관, 1대1문의, 회원탈퇴 */
+                    VStack {
+                        
+                        MoreInfoListItem(title: "공지사항")
+                        GrayLine()
+                        
+                        MoreInfoListItem(title: "1대1문의", subTitle: "불편한점이나 바라는점을 말씀해주세요")
+                        GrayLine()
+                        
+                        MoreInfoListItem(title: "이용약관")
+                        GrayLine()
+                    }
+                    .padding(.bottom, 15)
+                    
+                }
+                Spacer()
+                /* Footer */
+                HStack {
+                    Spacer()
+                    Button {
+                        // To-Do: 로그아웃 구현
+                        print("로그아웃 되었습니다.")
+                        showLogOutAlert = true
+                    } label: {
+                        Text("로그아웃")
+                            .font(.system(size: 15))
+                            .foregroundColor(Color.gray91)
+                    }
+                    .padding(.horizontal)
+                    
+                    Text(" | ")
                     
                     Button {
-
-                        print("계좌번호가 복사되었습니다.")
-                        
+                        // To-Do: 회원탈퇴 구현
+                        deleteAccount = true
                     } label: {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text(bankAccountBank)
-                                    .bold()
-                                    .font(.system(size: 13))
-                                    .foregroundColor(Color.grayAccount)
-                                    .padding(.bottom, 5)
-                                
-                                HStack {
-                                    Text(bankAccountNumber)
-                                        .font(.system(size: 13))
-                                        .foregroundColor(Color.grayAccount)
-                                    Text(bankAccountHolder)
-                                        .font(.system(size: 13))
-                                        .foregroundColor(Color.grayAccount)
-                                }
-                            }
-                            Spacer()
-                            Image(systemName: "doc.on.doc")
-                                .foregroundColor(.grayWithBlue)
-                        }
-                        .padding(20)
-                        .frame(width: 340, height: 90, alignment: .leading)
-                        .background(Color.grayF0)
-                        .cornerRadius(8)
+                        Text("회원탈퇴")
+                            .font(.system(size: 15))
+                            .foregroundColor(Color.gray91)
+                    }
+                    .padding(.horizontal)
+                    Spacer()
+                }
+                .padding(.bottom)
+                
+                HStack {
+                    Spacer()
+                    Text("돈워리 ver 1.0.0")
+                        .font(.system(size: 15))
+                        .foregroundColor(Color.gray91)
+                    Spacer()
+                }
+                .padding(.bottom)
+                
+            }
+            .padding(.top, -20)
+            .navigationBarBackButtonHidden(true)
+            .padding(.horizontal, 25)
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button {
+                        self.mode.wrappedValue.dismiss()
+                    } label: {
+                        Image(systemName: "chevron.left")
+                            .foregroundColor(.black)
+                            .padding(.horizontal)
                     }
                 }
-                .padding(.vertical, 10)
-                GrayLine()
-                   
-                /* Section 3 : 공지사항, 이용약관, 1대1문의, 회원탈퇴 */
-                VStack {
-                    
-                    MoreInfoListItem(title: "공지사항")
-                    GrayLine()
-                    
-                    MoreInfoListItem(title: "1대1문의", subTitle: "불편한점이나 바라는점을 말씀해주세요")
-                    GrayLine()
-                                  
-                    MoreInfoListItem(title: "이용약관")
-                    GrayLine()
-                }
-                .padding(.bottom, 15)
-                     
-            }
-            Spacer()
-            /* Footer */
-            HStack {
-                Spacer()
-                Button {
-                    // To-Do: 로그아웃 구현
-                    print("로그아웃 성공")
-                } label: {
-                    Text("로그아웃")
-                        .font(.system(size: 15))
-                        .foregroundColor(Color.gray91)
-                }
-                .padding(.horizontal)
-                
-                Text(" | ")
-                
-                Button {
-                    // To-Do: 회원탈퇴 구현
-                    print("회원탈퇴 성공")
-                } label: {
-                    Text("회원탈퇴")
-                        .font(.system(size: 15))
-                        .foregroundColor(Color.gray91)
-                }
-                .padding(.horizontal)
-                Spacer()
-            }
-            .padding(.bottom)
-            
-            HStack {
-                Spacer()
-                Text("돈워리 ver 1.0.0")
-                    .font(.system(size: 15))
-                    .foregroundColor(Color.gray91)
-                Spacer()
-            }
-            .padding(.bottom)
-            
-        }
-        .padding(.top, -20)
-        .navigationBarBackButtonHidden(true)
-        .padding(.horizontal, 25)
-        .toolbar {
-            ToolbarItem(placement: .cancellationAction) {
-                Button {
-                    self.mode.wrappedValue.dismiss()
-                } label: {
-                    Image(systemName: "chevron.left")
-                        .foregroundColor(.black)
-                        .padding(.horizontal)
-                }
-            }
-            ToolbarItem(placement: .confirmationAction) {
+                ToolbarItem(placement: .confirmationAction) {
                     Text("내 정보")
                         .font(.title2.weight(.bold))
                         .padding(.trailing)
+                }
             }
         }
+        .alert("회원탈퇴를 하시겠어요?", isPresented: $deleteAccount, actions: {
+            Button("확인", action: {
+                // 회원탈퇴 기능 구현
+                print("회원탈퇴 되었습니다.")
+            })
+            Button("취소", role: .cancel, action: {
+            }).keyboardShortcut(.defaultAction)
+        })
     }
 }
 
